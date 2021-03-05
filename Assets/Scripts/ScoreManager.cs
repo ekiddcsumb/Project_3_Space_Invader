4d0812cfc38cae3daeeae2aa9fb3a928f0099f60
emkidd <ekidd@csumb.edu>
2021-03-04 17:42:15 -0800

    manager scripts

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    void AddScore(int score)
+    {
+        Debug.Log("AddScore");
+    }
+
+    void StartGame()
+    {
+        Debug.Log("StartGame");
+    }
+
+    void EndGame()
+    {
+        Debug.Log("EndGame");
+    }
+}
